c9a91fa18bc29ea2c541794fdbb8f10fae6d233d
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-03-20 17:00:12 +0530

    Reding input from files in asynchronus way

--- 1-Hello_World/index.js
+++ 1-Hello_World/index.js
@@ -3,5 +3,18 @@ const fs = require('fs');
 const hello = 'Hello World';
 console.log(hello);
 
-const textIn = fs.readFileSync('./file.txt', 'utf-8');
-console.log(textIn);
+// Blocking Synchronus Way
+
+// const textIn = fs.readFileSync('./file.txt', 'utf-8');
+// console.log(textIn);
+
+// const textOut = `${textIn} This is extra string at the end of the text added on date ${Date.now()}`;
+// fs.writeFileSync('./file.txt', textOut);
+// console.log('File Written');
+
+// Non-Blocking Asynchronus Way
+fs.readFile('./file.text', 'utf-8' ,(err, data) => {
+    console.log(data)
+})
+
+console.log('Will read file');
