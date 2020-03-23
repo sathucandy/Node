d2b7141ec6e2142fd3aecddfd64fe40d23bca7e4
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-03-23 12:31:17 +0530

    Creating Modules

--- 5-Node-Farm/index.js
+++ 5-Node-Farm/index.js
@@ -1,6 +1,7 @@
 const url = require("url");
 const http = require("http");
 const fs = require("fs");
+const replaceTemplate = require("./modules/replaceTemplate");
 
 ///  WE READ THE FILE HERE INSTEAD OF "/api" BECAUSE HERE WE ARE READING
 // THE FILE ONLY ONCE AND CAN CALL DATA AS MANY TIME AS THE API IS HIT
@@ -21,22 +22,6 @@ const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
   "utf-8"
 );
-
-const replaceTemplate = (template, product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName); // HERE WE ARE REPLACING THE QUOTES WITH REGEX SO THAT MULTIPLE INSTANCES OF PRODUCTNAME GETS REPLACED
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-  return output;
-};
 
 const server = http.createServer((req, res) => {
   // console.log(req.url);
