c47549b892428b81d0a04b65c3f832482d185f6b
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-03-26 21:22:52 +0530

    Producing Promises

---
+++ 10-Promises/index.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+const superagent = require("superagent");
+
+// create a file function that read file name only and dont return any call back function
+const readFilePro = file => {
+  // promise constructor takes a funtion which is executed immediately once the constructor is called
+  // and take parameters resolve and reject
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("I could not find the file");
+      resolve(data);
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then(data => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then(res => {
+    console.log(res.body.message);
+
+    // this is to write into file and return a promise
+    return writeFilePro("dog-image.txt", res.body.message);
+    // fs.writeFile("dog-image.txt", res.body.message, err => {
+    //   console.log("Random dog image saved");
+    // });
+  })
+  .then(() => {
+    console.log("Random dog image saved");
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
+
+// writing file promise
+const writeFilePro = (file, data) => {
+  new Promise((resolve, reject) => {
+    fs.writeFile(file, data, err => {
+      if (err) reject("Could not write a file");
+      resolve("success");
+    });
+  });
+};
+
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//   console.log(`Breed: ${data}`);
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .then(res => {
+//       console.log(res.body.message);
+//       fs.writeFile("dog-image.txt", res.body.message, err => {
+//         console.log("Random dog image saved");
+//       });
+//     })
+//     .catch(err => {
+//       console.log(err.message);
+//     });
+// });
