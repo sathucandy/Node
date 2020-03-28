bd795afa404968b4481adf44dec2581e8830e959
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-03-28 19:42:47 +0530

    Handling get requests

--- 12-Natours/app.js
+++ 12-Natours/app.js
@@ -1,7 +1,25 @@
+const fs = require("fs");
 const express = require("express");
 
 // adding bunch of methods of express to app variable
 const app = express();
+
+// reading the json file data for getting tours
+// JSON.parse converts the jason data into javascript
+// object or an array of javascript object
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+});
 
 // creating a server in express
 const port = 3000;
@@ -10,13 +28,13 @@ app.listen(port, () => {
 });
 
 // defining route
-app.get("/", (req, res) => {
-  //   res.status(200).send("Hello from the server side");
-  res
-    .status(200)
-    .json({ message: "Hello from the server side", app: "Natours" });
-});
+// app.get("/", (req, res) => {
+//   //   res.status(200).send("Hello from the server side");
+//   res
+//     .status(200)
+//     .json({ message: "Hello from the server side", app: "Natours" });
+// });
 
-app.post("/", (req, res) => {
-  res.send("You can post to this endpoint");
-});
+// app.post("/", (req, res) => {
+//   res.send("You can post to this endpoint");
+// });
