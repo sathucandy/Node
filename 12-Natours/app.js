087a4bed2692ae32d393b21ce06a65ae835b3989
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-03-29 14:17:46 +0530

    Implementing patch

--- 12-Natours/app.js
+++ 12-Natours/app.js
@@ -72,6 +72,21 @@ app.post("/api/v1/tours", (req, res) => {
   // res.send("done");
 });
 
+app.patch("/api/v1/tours/:id", (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Not found"
+    });
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "Updated Tour here"
+    }
+  });
+});
+
 // creating a server in express
 const port = 3000;
 app.listen(port, () => {
