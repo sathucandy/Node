99ee4fd36c14cf33dff3c530d7f2aaab60f466ea
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-04-04 19:13:28 +0530

    Connecting to the database

--- 12-Natours/server.js
+++ 12-Natours/server.js
@@ -1,9 +1,28 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv"); // this is to read config.env file
 const app = require("./app");
-// console.log(app.get("env"));
-console.log(process.env);
+console.log(app.get("env"));
+
+dotenv.config({ path: "./config.env" }); // this is to read config.env file
+
+// connecting to mongoose
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true, // these parameters are for removing the warnings
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    // console.log(con.connections);
+    console.log("DB connections successful");
+  });
+
+// console.log(process.env);
 // 4) creating a server in express
 const port = 3000;
 app.listen(port, () => {
