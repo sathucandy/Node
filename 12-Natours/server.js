0f4c44bad8a2c03bc1cfcb526fc8f2aff8b4f9b3
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-04-04 19:26:30 +0530

    Creating schema

--- 12-Natours/server.js
+++ 12-Natours/server.js
@@ -22,6 +22,26 @@ mongoose
     console.log("DB connections successful");
   });
 
+// creating a schema using mongoose
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A Tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+
+// creating a model out of schema
+const Tour = mongoose.model("Tour", tourSchema); // first param is name of the model and second param is name of the schema
+
 // console.log(process.env);
 // 4) creating a server in express
 const port = 3000;
