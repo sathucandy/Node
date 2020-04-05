fcad0b593fc5cb6255e28f44512ad28e390ebb54
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-04-05 20:54:23 +0530

    Reading

--- 12-Natours/controllers/tourController.js
+++ 12-Natours/controllers/tourController.js
@@ -15,43 +15,61 @@ exports.checkBody = (req, res, next) => {
 
 // to make these functions available to other modules we will replace const with export
 
-exports.getAllTours = (req, res) => {
-  // calling the middlewear
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: "success",
-    requestedAt: req.requestTime,
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    console.log(req.requestTime);
+    res.status(200).json({
+      status: "success",
+      requestedAt: req.requestTime,
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(401).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  console.log(req.params);
-  // here we are converting id to integer as it returns string
-  // when we multiply a string by number it converts string to number
-  const id = req.params.id * 1;
-  // // const tour = tours.find((el) => {
-  // //   return el.id === id;
-  // // });
-  // // res.status(200).json({
-  // //   status: "success",
-  // //   data: {
-  // //     tour,
-  // //   },
-  // });
+exports.getTour = async (req, res) => {
+  try {
+    // Tour.findOne({_id:req.params.id})
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(401).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
-exports.createTour = (req, res) => {
-  // console.log(req.body);
-  res.status(201).json({
-    status: "success",
-    data: {
-      // tour: newTour,
-    },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({});
+    // newTour.save()
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "Failed",
+      message: "Invalid data sent",
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
