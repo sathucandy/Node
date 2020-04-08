9b00eec18f3cecaa2f846c4b3a10db5a4d9a0ffa
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-04-08 12:40:48 +0530

    adding limiting fields api

--- 12-Natours/controllers/tourController.js
+++ 12-Natours/controllers/tourController.js
@@ -41,6 +41,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort("-createdAt");
     }
 
+    // 3) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
     // {difficulty:"easy",duration:{$gte: 5}}
     // const query =  Tour.find()
     //   .where("duration")
