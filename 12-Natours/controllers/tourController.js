30545a2a122779bdaf08bc19fdd15d73c9ad1314
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-04-13 23:32:33 +0530

    Refactoring the code

--- 12-Natours/controllers/tourController.js
+++ 12-Natours/controllers/tourController.js
@@ -1,5 +1,5 @@
 const Tour = require("./../models/tourModel");
-
+const APIFeautures = require("./../utils/apiFeatures");
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
@@ -25,44 +25,6 @@ exports.aliasTopTours = (req, res, next) => {
 exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
-    // Build the query
-    // 1A) Filtering
-    const queryObj = { ...req.query }; // here this syntax is defining a new object which is a copy of req.query otherwise the object would have been referred
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((el) => {
-      delete queryObj[el];
-    });
-    // 1B) Advanced Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 2) Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      console.log(sortBy);
-      query = query.sort(sortBy);
-      // sort("price ratingsAverage")
-    } else {
-      query = query.sort("-createdAt");
-    }
-
-    // 3) Field Limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
-    } else {
-      query = query.select("-__v");
-    }
-
-    // 4) Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    // page=2&limit=10, 1-10, page 1, 11-20, page 2, ...
-    query = query.skip(skip).limit(limit);
-
     // {difficulty:"easy",duration:{$gte: 5}}
     // const query =  Tour.find()
     //   .where("duration")
@@ -70,8 +32,14 @@ exports.getAllTours = async (req, res) => {
     //   .where("difficulty")
     //   .equals("easy");
     // console.log(req.requestTime);
-    // Execute the query
-    const tours = await query;
+
+    // EXECUTE THE QUERY
+    const features = new APIFeautures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
     // SEND RESPONSE
     res.status(200).json({
       status: "success",
