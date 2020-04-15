7f7ebf61a3195323089b84ac08aaf09a86d2b6a9
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-04-15 12:15:14 +0530

    Adding document middlewear

--- 12-Natours/models/tourModel.js
+++ 12-Natours/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 // creating a schema using mongoose
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, "A Tour must have a duration"],
@@ -63,6 +65,23 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWEAR: runs before .save() and .create()
+tourSchema.pre("save", function (next) {
+  // console.log(this);
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre("save", function(next){
+//   console.log("Will save the document");
+//   next();
+// })
+
+// tourSchema.post("save", function(doc,next){
+//   console.log(doc);
+//   next();
+// })
 
 // creating a model out of schema
 const Tour = mongoose.model("Tour", tourSchema); // first param is name of the model and second param is name of the schema
